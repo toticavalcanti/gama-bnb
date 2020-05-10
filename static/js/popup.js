5b8007ac2e85a6d0424ab09c770256bb16cfcc3c
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-05-10 20:47:33 -0300

    fix modal

--- static/js/popup.js
+++ static/js/popup.js
@@ -3,6 +3,6 @@ function popup(mylink, windowname) {
     var href;
     if (typeof(mylink) == 'string') href = mylink;
     else href = mylink.href;
-    window.open(href, windowname, 'width=400,height=200,scrollbars=yes');
+    window.open(href, windowname, 'width=600,height=400,scrollbars=yes');
     return false;
 }
